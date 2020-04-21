4db54cd9c2c3bd1a3fd6279c11a7086a73fc5159
Jarome Leslie <jsleslie@users.noreply.github.com>
2020-04-20 23:05:22 -0700

    in progress dash

--- app.py
+++ app.py
@@ -39,64 +39,65 @@ colors = {"white": "#ffffff",
 
 
 # APP LAYOUT
-app.layout = html.Div(style={'backgroundColor': colors['light_grey']}, children=[
+app.layout = html.Div(style={'backgroundColor': colors['light_grey'], 'margin-left':0, 'margin-right':0}, children=[
     # HEADER
-    html.Div(className="row", style={'backgroundColor': colors['black'], "padding": 10}, children=[
+    html.Div(className="row", style={'backgroundColor': colors['black'], "padding": 10, 'margin-left':0, 'margin-right':0}, children=[
         html.H2('Language Transliteration App',
                 style={'color': colors['white']})
     ]),
 
     # SELECTION HEADER 2
-    html.Div(className="row", style={'backgroundColor': colors['light_grey'], "padding": 10}, children=[
-        html.Div(className="two columns", style={'padding': 20}, children=[
+    html.Div(className="row", style={'backgroundColor': colors['light_grey'], "padding": 10, 'margin-left':0, 'margin-right':0}, children=[
+        html.Div(className="two columns", style={'padding': 20, 'margin-left':0, 'margin-right':0}, children=[
         html.P('Source Language:',
-                style={'color': colors['black']}),
-        html.Br(),
-        html.Label("Song name:")        
+                style={'color': colors['black']})
+        # html.Br()
+        # html.Label("Song name:")        
                 ]),
 
-        html.Div(className="ten columns", style={'padding': 20}, children=[        
+        html.Div(className="ten columns", style={'padding': 20, 'margin-left':0, 'margin-right':0}, children=[        
             dcc.Dropdown(
-                            id = 'pollutant1',
+                            id = 'mode1',
                             options=[
-                                {'label': 'Mandarin to Pinyin', 'value': 1},
-                                {'label': 'Cantonese to XXX', 'value': 2}
+                                {'label': 'Mandarin to Pinyin', 'value': 1}
                             ],
                             value = 1,
-                            style={'height': '30px', 'width': '300px'}),
-            html.Br(),
-             dcc.Input(id="topic_title", placeholder="Enter song name",
-                      type="text", size="75", value="")
+                            style={'height': '30px', 'width': '300px'})
+            # html.Br()
+            #  dcc.Input(id="topic_title", placeholder="Enter song name",
+                    #   type="text", size="75", value="")
                     
     ])]),
 
     # MAIN BODY
-    html.Div(className="row", children=[
+    html.Div(className="row", style={'margin-left':0, 'margin-right':0}, children=[
         # # SIDEBAR
         # html.Div(className="two columns", style={'padding': 20}, children=[
         #     html.Img(src="assets/ubc-logo-2.png", width="50"),
         #     html.P("Home\nAnnouncements\nDiscussions\nGrades\nPeople")
         # ]),
         # DISCUSSION BOARD
-        html.Div(className="six columns", style={"backgroundColor": colors['white'], "padding": 20}, children=[
+        html.Div(className="six columns", style={"backgroundColor": colors['white'], "padding": 20, 'margin-left':0, 'margin-right':0}, children=[
     
-            html.Br(),
-            html.Br(),
-            html.Label("Source lyrics:"),
-            dcc.Input(id="topic_message", placeholder="Song lyrics",
+            # html.Br(),
+            # html.Br(),
+            html.Label("Paste song text here:"),
+            dcc.Input(id="text_input", placeholder="Song lyrics",
                       type="text", size="75", style={'height': 250}),
             html.Br(),
             html.Br(),
 
         ]),
-        html.Div(className="six columns", style={"backgroundColor": colors['white'], "padding": 20}, children=[
+        html.Div(className="six columns", style={"backgroundColor": colors['white'], "padding": 20, 'margin-left':0, 'margin-right':0}, children=[
     
 
-            html.Br(),
-            html.Br(),
+            # html.Br(),
+            # html.Br(),
             html.Label("Model-generated lyrics:"),
-            dcc.Input(id="topic_message2", placeholder="Song lyrics",
-                      type="text", size="75", style={'height': 250}),
+            html.P("Generated pinyin", id = "text_out", style={"backgroundColor": colors['light_grey'], 'border': '1px solid', "padding-left": 5}),
+
+            # dcc.Input(id="generated_output", placeholder="Song lyrics",
+                    #   type="text", size="75", style={'height': 250}),
             html.Br(),
             html.Br(),
 
@@ -109,3 +110,15 @@ app.layout = html.Div(style={'backgroundColor': colors['light_grey']}, children=
 
 if __name__ == '__main__':
     app.run_server(debug=False)
+
+
+@app.callback(
+    [dash.dependencies.Output('text_out', 'children')],
+    [dash.dependencies.Input('mode1', 'value'),
+     dash.dependencies.Input('text_input', 'children')])
+def generate_text(mode1, text_input):
+
+    # if mode1 == 1:
+    text_out = text_input
+
+    return text_out
